4d16a2e9b4905ff8c66659019d7fa12ec7588af4
Soumya Deep Das <soumyadeepdas.beas@gmail.com>
2023-08-01 18:33:11 +0530

    soumya 01-08-2023

--- aiJS/movecar.js
+++ aiJS/movecar.js
@@ -44,71 +44,71 @@ function moveFront(margHori) {
     console.log(document.getElementsByClassName("car")[0].style.marginLeft);
 }
 
-let marginSpeed = 0;
-function speedRight(margHori) {
-    if (margHori == "") {
-        margHori = "200px";
-    }
-    let marginH = margHori;
-    let findInd = margHori.search("px");
-    let marginAmount = parseInt(marginH.substring(0, findInd));
-    marginSpeed = marginAmount
-    setInterval(() => {
-        if (marginSpeed < 500) {
-            marginSpeed += 2;
-        }
-        if (marginSpeed <= 0) {
-            marginSpeed += 4;
-        }
-        document.getElementsByClassName("car")[0].style.marginLeft = marginSpeed + "px";
-        console.log(document.getElementsByClassName("car")[0].style.marginLeft);
-    }, 50)
-}
-function speedLeft(margHori) {
-    if (margHori == "") {
-        margHori = "200px";
-    }
-    let marginH = margHori;
-    let findInd = margHori.search("px");
-    let marginAmount = parseInt(marginH.substring(0, findInd));
-    marginSpeed = marginAmount
-    setInterval(() => {
-        if (marginSpeed < 450) {
-            marginSpeed -= 4;
-        }
-        if (marginSpeed <= 0) {
-            marginSpeed = 0;
-        }
-        document.getElementsByClassName("car")[0].style.marginLeft = marginSpeed + "px";
-        console.log(document.getElementsByClassName("car")[0].style.marginLeft);
-    }, 500)
-}
-let marginSpeedN = 0;
-function speedDown(margTop) {
-    if (margTop == "") {
-        margTop = "80px";
-    }
-    let marginT = margTop;
-    let findInd = margTop.search("px");
-    let marginAmount = parseInt(marginT.substring(0, findInd));
-    marginSpeedN = marginAmount
-    setInterval(() => {
-        marginSpeedN += 1;
-        document.getElementsByClassName("car")[0].style.marginTop = marginSpeedN + "px";
-        console.log(document.getElementsByClassName("car")[0].style.marginTop);
-    }, 170)
-}
-function speedUp(margTop) {
-    if (margTop == "") {
-        margTop = "80px";
-    }
-    let marginT = margTop;
-    let findInd = margTop.search("px");
-    let marginAmount = parseInt(marginT.substring(0, findInd));
-    marginSpeedN = marginAmount
-    setInterval(() => {
-        marginSpeedN -= 1;
-        document.getElementsByClassName("car")[0].style.marginTop = marginSpeedN + "px";
-        console.log(document.getElementsByClassName("car")[0].style.marginTop);
-    }, 170)
-}
+// let marginSpeed = 0;
+// function speedRight(margHori) {
+//     if (margHori == "") {
+//         margHori = "200px";
+//     }
+//     let marginH = margHori;
+//     let findInd = margHori.search("px");
+//     let marginAmount = parseInt(marginH.substring(0, findInd));
+//     marginSpeed = marginAmount
+//     setInterval(() => {
+//         if (marginSpeed < 500) {
+//             marginSpeed += 1.2;
+//         }
+//         if(marginSpeed>=500) {
+//             clearInterval();
+//         }
+//         document.getElementsByClassName("car")[0].style.marginLeft = marginSpeed + "px";
+//         console.log(document.getElementsByClassName("car")[0].style.marginLeft);
+//     }, 50)
+// }
+// function speedLeft(margHori) {
+//     if (margHori == "") {
+//         margHori = "200px";
+//     }
+//     let marginH = margHori;
+//     let findInd = margHori.search("px");
+//     let marginAmount = parseInt(marginH.substring(0, findInd));
+//     marginSpeed = marginAmount
+//     setInterval(() => {
+//         if (marginSpeed < 500) {
+//             marginSpeed -= 0.8;
+//         }
+//         if(marginSpeed>=500) {
+//             clearInterval();
+//         }
+//         document.getElementsByClassName("car")[0].style.marginLeft = marginSpeed + "px";
+//         console.log(document.getElementsByClassName("car")[0].style.marginLeft);
+//     }, 500)
+// }
+// let marginSpeedN = 0;
+// function speedDown(margTop) {
+//     if (margTop == "") {
+//         margTop = "80px";
+//     }
+//     let marginT = margTop;
+//     let findInd = margTop.search("px");
+//     let marginAmount = parseInt(marginT.substring(0, findInd));
+//     marginSpeedN = marginAmount
+//     setInterval(() => {
+//         marginSpeedN += 1;
+//         document.getElementsByClassName("car")[0].style.marginTop = marginSpeedN + "px";
+//         console.log(document.getElementsByClassName("car")[0].style.marginTop);
+//     }, 170)
+// }
+// function speedUp(margTop) {
+//     if (margTop == "") {
+//         margTop = "80px";
+//     }
+//     let marginT = margTop;
+//     let findInd = margTop.search("px");
+//     let marginAmount = parseInt(marginT.substring(0, findInd));
+//     marginSpeedN = marginAmount
+//     setInterval(() => {
+//         marginSpeedN -= 1;
+//         document.getElementsByClassName("car")[0].style.marginTop = marginSpeedN + "px";
+//         console.log(document.getElementsByClassName("car")[0].style.marginTop);
+//     }, 170)
+// }
