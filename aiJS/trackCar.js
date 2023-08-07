06a56072d312be2541e6a76e1ec489f891d34adc
Soumya Deep Das <soumyadeepdas.beas@gmail.com>
2023-08-07 19:07:07 +0530

    somya push 07-08-2023

--- aiJS/trackCar.js
+++ aiJS/trackCar.js
@@ -5,7 +5,28 @@ function init() {
     objImage.style.left = "0px";
     objImage.style.top = "0px";
 }
+
 function getKeyAndMove(e) {
+    if (e.key == "a") {
+        moveLeft()
+    }
+    if (e.key == "w") {
+        moveUp()
+    }
+    if (e.key == "w" && e.key == "a") {
+        moveLeft()
+        moveUp()
+    }
+    if (e.key == "s") {
+        moveDown()
+    }
+    if (e.key == "d") {
+        moveRight()
+    }
+    if (e.key == "d" && e.key == "s") {
+        moveDown();
+        moveRight();
+    }
     var key_code = e.which || e.keyCode;
     switch (key_code) {
         case 37: //left arrow key
@@ -22,28 +43,31 @@ function getKeyAndMove(e) {
             break;
     }
 }
-function moveLeft() {
-    objImage.style.left = parseInt(objImage.style.left) - 5 + "px";
-}
+
 function moveUp() {
+    objImage.style.top = parseInt(objImage.style.top) - 5 + "px";
+}
+function moveLeft() {
     let searchdeg = objImage.style.top.search("px");
     let degFrompx = objImage.style.top.substring(0, searchdeg);
     let coordUp = parseInt(degFrompx) - 5 + "deg";
     objImage.style.transform = `rotate(${coordUp})`;
-    let searchcoord = coordUp.search("deg");
-    let pxFromdeg = coordUp.substring(0, searchcoord);
-    objImage.style.top = pxFromdeg + "px";
-}
-function moveRight() {
-    objImage.style.left = parseInt(objImage.style.left) + 5 + "px";
+    // let searchcoord = coordUp.search("deg");
+    // let pxFromdeg = coordUp.substring(0, searchcoord);
+    // objImage.style.left = pxFromdeg + "px";
+    objImage.style.left = parseInt(objImage.style.left) - 5 + "px";
 }
 function moveDown() {
+    objImage.style.top = parseInt(objImage.style.top) + 5 + "px";
+}
+function moveRight() {
     let searchdeg = objImage.style.top.search("px");
     let degFrompx = objImage.style.top.substring(0, searchdeg);
     let coordDown = parseInt(degFrompx) + 5 + "deg";
     objImage.style.transform = `rotate(${coordDown})`;
-    let searchcoord = coordDown.search("deg");
-    let pxFromdeg = coordDown.substring(0, searchcoord);
-    objImage.style.top = pxFromdeg + "px";
+    // let searchcoord = coordDown.search("deg");
+    // let pxFromdeg = coordDown.substring(0, searchcoord);
+    // objImage.style.left = pxFromdeg + "px";
+    objImage.style.left = parseInt(objImage.style.left) + 5 + "px";
 }
 window.onload = init;
